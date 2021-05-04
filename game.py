cd3314863b3bc1ae7f53332df7430e99ab24479e
20171062 <qlwmsldj@naver.com>
2021-05-04 21:20:38 +0900

    중간 시험제출

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
-print("테트리스 시작")
-list_a = ("1. 오른쪽", "2. 왼쪽", "3. 블럭 바꾸기", "4. 종료")
+print("테트리스를 시작하겠습니다.")
+
+list_a = ("1. 왼쪽", "2. 오른쪽", "3. 블럭 바꾸기", "4. 종료")
+
 print(list_a)
-dict_a = {1 : list_a[0],2 : list_a[1],3 : list_a[2],4 : list_a[3]}
+dict_a = {1 : list_a[0], 2 : list_a[1], 3 : list_a[2], 4 : list_a[3]}
+
 num = int(input("선택 : "))
-if num ==1:
+if num == 1:
     print(dict_a[1])
 elif num == 2:
     print(dict_a[2])
